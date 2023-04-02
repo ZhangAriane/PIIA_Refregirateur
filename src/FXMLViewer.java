74a82cbcc70e074fba2f7d192aa4d6715786a7ca
yangx219 <xuey648@gmail.com>
2023-04-02 19:24:27 +0200

    4 fxml + changer l'interface

--- src/FXMLViewer.java
+++ src/FXMLViewer.java
@@ -1,28 +1,63 @@
 package src;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FXMLViewer extends Application {
 
-	private static final String FXML_FILE_PATH = "/src/FXML/Liste_de_Course.fxml";
+	private static final String FXML_FILE_PATH1 = "/src/FXML/Ajoutez_un_Aliment.fxml";
+	private static final String FXML_FILE_PATH2 = "/src/FXML/Ajoutez_une_Recette.fxml";
+	private static final String FXML_FILE_PATH3 = "/src/FXML/Liste_de_Course.fxml";
+	private static final String FXML_FILE_PATH4 = "/src/FXML/Liste_de_Recettes.fxml";
 
-	public FXMLViewer() {
-		// 无参构造函数
-	}
+	private Scene scene1;
+	private Scene scene2;
+	private Scene scene3;
+	private Scene scene4;
+	@FXML
+	private Button validerButton;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource(FXML_FILE_PATH));
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
+		// Load FXML files and create scenes
+		Parent root1 = FXMLLoader.load(getClass().getResource(FXML_FILE_PATH1));
+		scene1 = new Scene(root1);
+
+		Parent root2 = FXMLLoader.load(getClass().getResource(FXML_FILE_PATH2));
+		scene2 = new Scene(root2);
+
+		Parent root3 = FXMLLoader.load(getClass().getResource(FXML_FILE_PATH3));
+		scene3 = new Scene(root3);
+
+		Parent root4 = FXMLLoader.load(getClass().getResource(FXML_FILE_PATH4));
+		scene4 = new Scene(root4);
+
+		// Set scene1 as the initial scene
+		primaryStage.setScene(scene2);
 		primaryStage.show();
 	}
 
-	public static void main(String[] args) {
+	@FXML
+	private void handleValidation(ActionEvent event) throws IOException {
+		// Load Liste_de_Recettes.fxml and set it as the new scene
+		Parent root = FXMLLoader.load(getClass().getResource("/src/FXML/Liste_de_Recettes.fxml"));
+		Scene scene = new Scene(root);
+
+		Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+
+		public static void main(String[] args) {
 		launch(args);
 	}
 }
